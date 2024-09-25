e2cfde4235d8f43cb53ed8ff5e328bc00fa8f0bd
jacopolegovini <legovini96@gmail.com>
2024-09-25 17:15:05 +0200

    MainPlayers section completed

--- src/components/MainPlayers.vue
+++ src/components/MainPlayers.vue
@@ -40,11 +40,11 @@ export default {
 
 <template>
 <div class="main-players">
-        <div class="container">
-            <div class="title">
+        <div class="container container-flex">
+            <div class="title-section">
                 <h3>Players</h3>
             </div>
-            <ul class="player-cards">
+            <ul class="player-cards-section">
                 <li class="single-card" v-for="(player, index) in players" :key="index">
                     <div class="card-image">
                         <img :src='getImageUrl(`../assets/${player.img}`)' alt="player-img">
@@ -59,8 +59,10 @@ export default {
                     </div>
                 </li>
             </ul>
-            <div class="player-button">
-
+            <div class="player-button-section">
+                <div class="player-button">
+                    <p>View All Players</p>
+                </div>
             </div>
         </div>
 </div>
@@ -68,27 +70,39 @@ export default {
 
 <style lang="scss">
 .main-players {
-    height: 500px;
+    height: 550px;
     background-image: url(../assets/player.jpg);
     background-position: center;
+}
+
+.container-flex {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 30px;
 }
 
 img {
     width: 250px;
 }
 
+p {
+    margin: 0;
+}
+
 li {
     list-style-type: none;
 }
 
-.title {
+.title-section {
     color: white;
     font-weight: 500;
     text-align: center;
-    padding: 20px;
+
 }
 
-.player-cards {
+.player-cards-section {
     display: flex;
     justify-content: space-around;
 }
@@ -102,7 +116,10 @@ li {
 }
 
 .player-description {
-    text-align: center;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     color: white;
     width: 250px;
     height: 80px;
@@ -112,6 +129,27 @@ li {
     position: absolute;
     bottom: -10px;
     left: 0;
+}
+
+.name-role {
+    font-size: 0.9rem;
+    opacity: 0.8;
+}
+
+.player-button-section {
+    display: flex;
+    justify-content: center;
+}
+
+.player-button {
+    padding: 18px 20px;
+    background-color: black;
+    color: white;
+    width: 200px;
+    border-radius: 25px;
+    text-align: center;
+    border: 1px solid white;
+    font-weight: 500;
 }
 
 </style>
