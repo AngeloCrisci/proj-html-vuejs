9abc17485a495581bbb4711877d04d840f59c212
jacopolegovini <legovini96@gmail.com>
2024-09-25 16:20:09 +0200

    section structured

--- src/components/MainPlayers.vue
+++ src/components/MainPlayers.vue
@@ -13,7 +13,25 @@ export default {
 </script>
 
 <template>
-    <h3>MainPlayers</h3>
+<div class="main-players">
+        <div class="container">
+            <div class="title">
+                <h3>Players</h3>
+            </div>
+            <div class="player-cards">
+                <div class="card"></div>
+            </div>
+            <div class="player-button">
+
+            </div>
+        </div>
+</div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.main-players {
+    height: 500px;
+    background-image: url(../assets/player.jpg);
+    background-position: center;
+}
+</style>
