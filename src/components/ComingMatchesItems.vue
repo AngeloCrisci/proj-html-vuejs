c760c13c0af2e43fd26f3e841e50e5e42c5e32ca
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-26 12:31:53 +0200

    Re-added Angelo branch in another branch

---
+++ src/components/ComingMatchesItems.vue
@@ -0,0 +1,76 @@
+<script>
+import { store } from '../store'
+
+
+export default {
+    name: 'ComingMatchesItems',
+    data() {
+        return {
+            store,
+            teamsMatches: [
+                { name: store.teams[2].name, image: store.teams[2].img, nameTwo: store.teams[3].name, imageTwo: store.teams[3].img, date: 'October 20, 2022 , 17:00 pm' },
+                { name: store.teams[8].name, image: store.teams[8].img, nameTwo: store.teams[5].name, imageTwo: store.teams[5].img, date: 'September 18, 2022, 21:00 pm' },
+                { name: store.teams[1].name, image: store.teams[1].img, nameTwo: store.teams[7].name, imageTwo: store.teams[7].img, date: 'August 3, 2022, 19:00 pm' },
+                { name: store.teams[6].name, image: store.teams[6].img, nameTwo: store.teams[8].name, imageTwo: store.teams[8].img, date: 'July 8, 2022, 12:00 pm' },
+                { name: store.teams[0].name, image: store.teams[0].img, nameTwo: store.teams[1].name, imageTwo: store.teams[1].img, date: 'June 12, 2022, 09:00 am' },
+            ]
+        }
+
+    },
+    methods: {
+        getImageUrl(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <div v-for="(match, index) in teamsMatches" :key="index" class="cards-coming-matches">
+            <div class="mt-5 d-flex justify-content-between card-coming-matches">
+                <div class=" d-flex">
+                    <img :src="getImageUrl(`../assets/${match.image}`)" alt="Image">
+                    <p class="fw-bolder d-flex justify-content-center align-items-center"> {{ match.name }}</p>
+                </div>
+                <div>
+                    <p class="fw-bolder add-on"> VS</p>
+                </div>
+                <div class=" d-flex">
+                    <p class="fw-bolder d-flex justify-content-center align-items-center"> {{ match.nameTwo }}</p>
+                    <img :src="getImageUrl(`../assets/${match.imageTwo}`)" alt="Image">
+                </div>
+            </div>
+            <div class="address">
+                <div class="d-flex justify-content-center align-content-center">
+                    <p class="text-end"> {{ match.date }} </p>
+                    <p># Expert Stadium</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+p {
+    color: white;
+    margin-bottom: 0;
+}
+
+.card-coming-matches {
+    background-color: rgba(0, 0, 0, 0.158);
+}
+
+.address {
+    background-color: rgba(0, 0, 0, 0.356);
+    text-align: center;
+}
+
+img {
+    width: 60px;
+}
+
+.add-on {
+    font-size: 50px;
+}
+</style>
