366671f7b649b0c16fc8f1784810ed270e3b498a
jacopolegovini <legovini96@gmail.com>
2024-09-26 10:50:59 +0200

    mainSponsor completed

--- src/components/MainSponsor.vue
+++ src/components/MainSponsor.vue
@@ -26,18 +26,39 @@ export default {
 
 <template>
 <div class="main-sponsors">
-        <div class="container">
-            <div class="sponsor" v-for="(sponsor, index) in sponsors">
+        <div class="container container-flex">
+            <div class="sponsor" v-for="(sponsor, index) in sponsors" :key="index">
                 <img :src="getImageUrl(`../assets/${sponsor}`)" :alt="sponsor">
             </div>
         </div>
 </div>
 </template>
 
-<style lang="scss">
-
+<style lang="scss" scoped >
 .main-sponsors {
-    height: 250px;
+    height: 225px;
     background-color: black;
+    display: flex;
+    align-items: center;
+}
+
+.container-flex {
+    display: flex;
+    height: 60%;
+    justify-content: space-around;
+}
+
+.sponsor {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    gap: 20px;
+}
+
+img {
+    width: 70%;
+    border: 1px solid white;
+    border-radius: 30px;
+    padding: 20px;
 }
 </style>
