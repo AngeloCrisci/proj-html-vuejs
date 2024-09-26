27219b663a9bceb4a3616561d2b9e064b67c5c53
GiorgioDordo <djole6789@gmail.com>
2024-09-26 17:47:15 +0200

    layout MainArticles

--- src/components/MainArticles.vue
+++ src/components/MainArticles.vue
@@ -13,7 +13,76 @@ export default {
 </script>
 
 <template>
+<div class="container d-flex flex-column justify-content-center align-items-center mt-5">
+    <div class="text-center">
+        <i class="bi bi-hand-thumbs-up-fill h1"></i>
+        <p class="h2"><strong>Resent New & Articles</strong></p>
+        <p>Be aware of the football world.</p>
+    </div>
+    <div class="container text-center">
+        <div class="row align-items-center">
+            <div class="col-3">
+                <div class="card card-layout">
+                    <div class="card-body text-card-field text-center p-4">
+                        <img src="../assets/news4-1.png" class="card-img-top" alt="...">
+                        <p class="h4">29.05.2022-Football</p>
+                        <p class="h3"><strong>Football Terms</strong></p>
+                        <p>GOLDEN GOAL: In a football match, the goal scored by one of the teams in[...]</p>
+                        <button type="button" class="btn btn-dark button-card"><strong>More</strong></button>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-3">
+                <div class="card card-layout">
+                    <div class="card-body text-card-field text-center p-4">
+                        <img src="../assets/news4-1.png" class="card-img-top" alt="...">
+                        <p class="h4">29.05.2022-Football</p>
+                        <p class="h3"><strong>Football Terms</strong></p>
+                        <p>GOLDEN GOAL: In a football match, the goal scored by one of the teams in[...]</p>
+                        <button type="button" class="btn btn-dark button-card"><strong>More</strong></button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card card-layout">
+                    <div class="card-body text-card-field text-center p-4">
+                        <img src="../assets/news4-1.png" class="card-img-top" alt="...">
+                        <p class="h4">29.05.2022-Football</p>
+                        <p class="h3"><strong>Football Terms</strong></p>
+                        <p>GOLDEN GOAL: In a football match, the goal scored by one of the teams in[...]</p>
+                        <button type="button" class="btn btn-dark button-card"><strong>More</strong></button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card card-layout">
+                    <div class="card-body text-card-field text-center p-4">
+                        <img src="../assets/news4-1.png" class="card-img-top" alt="...">
+                        <p class="h4">29.05.2022-Football</p>
+                        <p class="h3"><strong>Football Terms</strong></p>
+                        <p>GOLDEN GOAL: In a football match, the goal scored by one of the teams in[...]</p>
+                        <button type="button" class="btn btn-dark button-card"><strong>More</strong></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.card-layout {
+    width: 20rem;
+    img{
+        height: 230px;
+        width: 220px;
+    }
+    .button-card {
+        padding: 20px 50px;
+        border-radius: 30px;
+    }
+}
+</style>
